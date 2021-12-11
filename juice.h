9ed83d9ad84c6c83b93ee6ce3dd3aecc4cf1d54f
krishpranav <krisna.pranav@gmail.com>
2021-12-11 18:50:39 +0530

    juice header
    object + errror + program + traceback
    error type

--- juice.h
+++ juice.h
@@ -24,3 +24,26 @@ extern "C"
 #define JUICE_VERSION_MINOR 2
 #define JUICE_VERSION_PATCH 0
 #define JUICE_VERSION_STRING "1.0.0"
+
+/**
+ * @brief object + errror + program + traceback
+ * 
+ */
+typedef struct juice juice_t;
+typedef struct juice_object { uint16_t _internal; } juice_object_t;
+typedef struct juice_error juice_error_t;
+typedef struct juice_program juice_program_t;
+typedef struct juice_traceback juice_traceback_t;
+
+/**
+ * @brief errors
+ * 
+ */
+typedef enum juice_error_type {
+    JUICE_ERROR_NONE = 0,
+    JUICE_ERROR_PARSING,
+    JUICE_ERROR_COMPILATION,
+    JUICE_ERROR_RUNTIME,
+    JUICE_ERROR_ALLOCATION,
+    JUICE_ERROR_USER,
+} juice_error_type_t;
