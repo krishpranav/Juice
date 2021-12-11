c145de9a97ee3263be33a620040b942c3ee5de5a
krishpranav <krisna.pranav@gmail.com>
2021-12-11 18:40:29 +0530

    juice header

---
+++ juice.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __cplusplus
+extern "C"
+{
+#endif
+#if 0
+}
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#ifndef _MSC_VER
+#define __attribute__(x)
+#endif
+
+/**
+ * @brief define juice version 
+ * 
+ */
+#define JUICE_VERSION_MAJOR 1
+#define JUICE_VERSION_MINOR 2
+#define JUICE_VERSION_PATCH 0
+#define JUICE_VERSION_STRING "1.0.0"
